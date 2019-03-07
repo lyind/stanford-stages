02e18a948f59ae9c32d1eee34e775443de0854de
Lars Köhler <lk@somnomedics.de>
2019-03-07 15:53:12 +0000

    fixing the cleanup after download
    Former-commit-id: b3664bca0724a5d9287fa27f6d3b4e62fae403c9

--- evaluate_service.py
+++ evaluate_service.py
@@ -1,6 +1,8 @@
 import os
-from bottle import Bottle, route, request, run, static_file
+from bottle import Bottle, route, request, run, static_file, response, install
 import inf_narco_app
+from zipfile import ZipFile
+import uuid
 
 app = Bottle()
 
@@ -11,28 +13,41 @@ def copy_filelike_to_filelike(src, dst, bufsize=16384):
             break
         dst.write(buf)
 
+
+def do_cleanup(file_uuid):
+    to_clean = ['.edf','.pkl','.hypno_pkl','.anl','.png','.hypnogram','.zip']
+    for i in to_clean:
+        try:
+            os.remove(file_uuid + i)
+        except:
+            continue
+
+
+def reply(filename):
+    return static_file(filename, root='.', download=True)
+
+
 @app.route('/api', method='POST')
 def evaluation():
     src = request.body
-    with open("to_evaluate.edf", "wb") as upload:
+    random_name = str(uuid.uuid1())
+    
+    @app.hook('after_request')
+    def cleaning_up():
+        print('cleaning files')
+        do_cleanup(random_name)
+    
+    with open(random_name + ".edf", "wb") as upload:
         copy_filelike_to_filelike(src, upload)
-    form = request.query.form
-    if form == "txt":
-        result = "to_evaluate.hypnogram"
-        dlname = "hypnogram.txt"
-        inf_narco_app.main("to_evaluate.edf", {"save":{"hypnogram":True}})
-    elif form == "png":
-        result = "to_evaluate.png"
-        dlname = "hypnodensity.png"
-        inf_narco_app.main("to_evaluate.edf", {"save":{"plot":True}})
-    else:
-        result = "to_evaluate.anl"
-        dlname = "profil.anl"
-        inf_narco_app.main("to_evaluate.edf", {})
-    os.remove("to_evaluate.edf")
-    os.remove("to_evaluate.pkl")
-    os.remove("to_evaluate.hypno_pkl")
-    return static_file(result, root='.', download=dlname)
+    
+    inf_narco_app.main(random_name + ".edf", {})
+    with ZipFile(random_name + ".zip", mode='w') as returnzip:
+        returnzip.write(random_name + ".anl")
+        returnzip.write(random_name + ".png")
+        returnzip.write(random_name + ".hypnogram")
+    
+    print('returning results')
+    return reply(random_name + '.zip')
 
 
 run(app, host='0.0.0.0', port=80, debug=True)
