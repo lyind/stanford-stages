a923b6b1c4be1d80b8abd14c15c90a7c97f3d505
Jonas Zeiger <jz@somnomedics.de>
2019-03-28 15:07:29 +0100

    serialize_anl(): implement 30% line width for REM stage

--- format/format_anl.py
+++ format/format_anl.py
@@ -8,11 +8,13 @@ def transform_anl_aasm(tuples):
     return np.nditer(np.argmax(tuples_reordered, axis=1))
 
 
-def serialize_anl(out_file, stages, starttime_seconds, epoch_duration_millies, colors=None):
+def serialize_anl(out_file, stages, starttime_seconds, epoch_duration_millies, colors=None, lineWidth=None):
     '''Serialize the values from the 1D generator stages into an ANL file for the specified recording start time.
 
        Colors may be specified in a list, where the index should match the associated value in stages.
        Colors are specified as little-endian RGB (without alpha).
+       
+       Line widths (replace event duration for sleep stage files only) may be specified in percent of graph height).
     '''
     
     # use default colorscheme if none has been specified
@@ -21,6 +23,12 @@ def serialize_anl(out_file, stages, starttime_seconds, epoch_duration_millies, c
         red = 0x000000FF
         colors = [red, red, red, blue, red, red]
 
+    # use line width
+    if lineWidth is None:
+        width_normal = 0x00000000
+        width_30percent = 0x1 * 1000 * 1000 * 60 * 60 * 24 * 30  # 30 days: 30% line width
+        colors = [width_normal, width_normal, width_normal, width_30percent, width_normal, width_normal]
+        
     # convert date from UNIX timestamps to "borland/excel days since 01-01-1900"
     # 25569 days between 01-01-1900 and 01-01-1970
     # 86400 seconds per day
@@ -35,6 +43,6 @@ def serialize_anl(out_file, stages, starttime_seconds, epoch_duration_millies, c
     timestamp_us = int(start_timestamp_us)
     for epoch_stage in stages:
         stage = int(epoch_stage)
-        out_file.write(serializer.pack(timestamp_us, 0, colors[stage], 0, stage, 0))
+        out_file.write(serializer.pack(timestamp_us, lineWidth[stage], colors[stage], 0, stage, 0))
         timestamp_us = timestamp_us + epoch_duration_us
 
